85dda19eb28573cce9f75a71ff9c6de8130f94ed
Clifton Snyder <cliff.snyder@gmail.com>
2012-08-26 15:39:02 -0400

    API is working for memory and load

--- dash-php/api/api.php
+++ dash-php/api/api.php
@@ -1,27 +1,10 @@
 <?php
 require("../config.php");
+include("../util.php");
 
 $rrds = array();
-
-// let's figure out which rrd we're trying to find
-$s = explode("?", $_SERVER["REQUEST_URI"]);
-$s = explode("/", $s[0]);
-$s = $s[count($s)-1];
-switch($s) {
-case "load": { 
-  $rrds["load"] = $data_dir . "/load/load.rrd"; 
-} break;
-case "memory": {
-  $rrds["buffered"] = $data_dir . "/memory/memory-buffered.rrd";
-  $rrds["cached"] = $data_dir . "/memory/memory-cached.rrd";
-  $rrds["free"] = $data_dir . "/memory/memory-free.rrd";
-  $rrds["used"] = $data_dir . "/memory/memory-used.rrd";
-} break;
-default: { 
-  header("HTTP/1.0 404 Not Found"); 
-  // TODO provide a 404 page
-}
-}
+// we might want some of these to have different names
+$renames = array();
 
 // options
 $cf = isset($_GET["cf"]) ? $_GET["cf"] : $DEFAULT_CF;
@@ -39,12 +22,37 @@ if(isset($_GET["r"])){
   $opts[] = $_GET["r"];
 }
 
+// let's figure out which rrd we're trying to find
+$s = explode("?", $_SERVER["REQUEST_URI"]);
+$s = explode("/", $s[0]);
+$s = $s[count($s)-1];
+switch($s) {
+case "load": { 
+  $rrds["load"] = $data_dir . "/load/load.rrd";
+} break;
+case "memory": {
+  $rrds["buffered"] = $data_dir . "/memory/memory-buffered.rrd";
+  $rrds["cached"] = $data_dir . "/memory/memory-cached.rrd";
+  $rrds["free"] = $data_dir . "/memory/memory-free.rrd";
+  $rrds["used"] = $data_dir . "/memory/memory-used.rrd";
+} break;
+default: { 
+  header("HTTP/1.0 404 Not Found"); 
+  // TODO provide a 404 page
+}
+}
+
 // grab the data from the RRDs
 $data = array();
 foreach($rrds as $key => $rrd) {
   $data[$key] = rrd_fetch($rrd, $opts);
 }
 
-// ...and spit it out as json
+// reorganize the data a bit
+$data = reorganize_data($data, $s);
+
+// TODO check Accept header, output XML if it's asked for
+// ...and spit it out
 echo json_encode($data);
+// echo xml_encode($data);
 ?>
